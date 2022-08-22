7b12d04318d285b32347c0b554648caf04866665
HelloEmo1337 <84297457+HelloEmo1337@users.noreply.github.com>
2022-08-22 09:30:07 +0300

    Add files via upload
    drvmap bilindik bir exe olduğu için ve kodu bulunmadığı için releasede bulabilirsiniz.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace inject
+{
+	// Token: 0x02000002 RID: 2
+	internal class Program
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		private static void Main(string[] args)
+		{
+			MessageBox.Show("UruedWasHere AkiraSpoofer...");
+			Process.Start("drvmap.exe", "spoofer.sys");
+		}
+	}
+}
